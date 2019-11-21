f60bab8a194474c3617dbcdcdb7813717df330c5
Josh Parkinson <joshparkinson1991@gmail.com>
2019-11-21 14:39:17 +0000

    feat: Linted typescript

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: "./tsconfig.json"
+    },
+    plugins: [
+        '@typescript-eslint'
+    ],
+    extends: [
+        'airbnb-base',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    ],
+    settings: {
+        "import/resolver": {
+            "node": {
+                "extensions": [
+                    ".ts"
+                ]
+            }
+        }
+    },
+    rules: {
+        "class-methods-use-this": "off",
+        "comma-dangle": ["error", "never"],
+        "indent": ["error", 4],
+        "max-len": ["error", {
+            "code": 120
+        }],
+
+        //Typescript overrides
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": ["error", {
+            "args": "none",
+        }],
+
+        "semi": "off",
+        "@typescript-eslint/semi": ["error"]
+    }
+
+};
