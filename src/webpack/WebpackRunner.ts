8338ea2567ab9684f7f6c3a41982a767afbf9bcd
Josh Parkinson <joshparkinson1991@gmail.com>
2019-12-17 10:18:28 +0000

    feat: Tighten up module resolution paths

--- src/webpack/WebpackRunner.ts
+++ src/webpack/WebpackRunner.ts
@@ -273,15 +273,21 @@ export default class WebpackRunner {
         const fontDir = this.string.trimRight(configData.structure.font_dir, '/');
         const imageDir = this.string.trimRight(configData.structure.image_dir, '/');
 
-        // Determine module resolution folders, this module may exist in a state
-        // where it's dependencies exist in the top level project node_modules
-        // directory or nested within it's own package directory. If its the
-        // latter than the nested directory must be searched when resolving
-        // modules. Use this for both loaders and modules.
-        const resolveModules = ['node_modules'];
+        // Determine module resolve paths.
+        // This package may exist where its dependencies exists in various
+        // locations. The following ensures the current directory and parent
+        // node_modules folders are searched. If a node_module directory exists
+        // at the application root it is searched. If a node_modules directory
+        // exists as a sibling of the resolve roo (usually the config file
+        // location) it is searched
+        let resolveModules = ['node_modules'];
         if (fs.existsSync(this.path.internal.resolve(appRoot, 'node_modules'))) {
             resolveModules.unshift(this.path.internal.resolve(appRoot, 'node_modules'));
         }
+        if (fs.existsSync(this.path.internal.resolve(resolveRoot, 'node_modules'))) {
+            resolveModules.unshift(this.path.internal.resolve(resolveRoot, 'node_modules'));
+        }
+        resolveModules = _.uniq(resolveModules);
 
         // Determine any resolve aliases
         const resolveAlias: { [key: string]: string } = {
